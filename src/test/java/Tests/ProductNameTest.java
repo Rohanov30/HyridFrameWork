09ab76efaf1de5fac2cef641996748cb708b4e33
Rohan <rohan_30nov@yahoo.com>
2026-02-12 13:01:30 +0530

    !2the Feb

--- src/test/java/Tests/ProductNameTest.java
+++ src/test/java/Tests/ProductNameTest.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -30,7 +31,7 @@ public class ProductNameTest extends BaseTest.Base {
 	    public void tearDown() {
 	        tearBrowser();
 	    }
-	    @Test(priority=1)
+	    @Test(enabled=false)
 	    public void checkItemNames() {
 	    	
 	    	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
@@ -44,7 +45,7 @@ public class ProductNameTest extends BaseTest.Base {
 	         textlist.stream().filter(s->s.contains("Sauce")).forEach(s->System.out.println(s));
 	    	
 	    }
-	    @Test(dependsOnMethods = {"checkItemNames"})
+	    @Test(enabled=false)
 	    public void checkPrice() {
 	    	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 	    	 LandingPage landingPage = new LandingPage(driver);
@@ -58,6 +59,46 @@ public class ProductNameTest extends BaseTest.Base {
 	    	
 	    	prices.stream().filter(s->s.endsWith("9")).forEach(s->System.out.println(s));
 	    	
+	    }
+	    @Test(priority=1)
+	    public void sortItemTest() {
+	    	
+	    	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	    	 LandingPage landingPage = new LandingPage(driver);
+	         landingPage.login("standard_user", "secret_sauce");
+	         Assert.assertEquals(driver.getCurrentUrl(),"https://www.saucedemo.com/inventory.html");
+	         List<WebElement> itemNames=driver.findElements(By.xpath("//div[@class='inventory_item_name ']"));
+	         
+	         List<String> textlist=itemNames.stream().map(s->s.getText()).collect(Collectors.toList());
+	         
+	         textlist.stream().sorted().forEach(s->System.out.println(s));
+	    	
+	    }
+	    @Test(priority=2)
+	    public void filterFunctionalitytest() {
+	    	
+	    	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	    	 LandingPage landingPage = new LandingPage(driver);
+	         landingPage.login("standard_user","secret_sauce");
+	         Assert.assertEquals(driver.getCurrentUrl(),"https://www.saucedemo.com/inventory.html");
+	         WebElement sel=driver.findElement(By.className("product_sort_container"));
+	       //  sel.click();
+	         Select select=new Select(sel);
+	         select.selectByVisibleText("Name (Z to A)");
+	         List<WebElement> itemNames=driver.findElements(By.xpath("//div[@class='inventory_item_name ']"));
+	         itemNames.stream().forEach(s->System.out.println(s.getText()));
+	         System.out.println("**********After Sorting**********");
+	         Select select2=new Select(sel);
+	         select2.selectByVisibleText("Name (A to Z)");
+	         List<WebElement> itemNames2=driver.findElements(By.xpath("//div[@class='inventory_item_name ']"));
+	         itemNames2.stream().forEach(s->System.out.println(s.getText()));
+	         
+	         System.out.println("**********Sorted List**********");
+	         itemNames.stream().sorted().forEach(s->System.out.println(s.getText()));
+	         Assert.assertEquals(itemNames.stream().sorted().collect(Collectors.toList()),
+	        		 itemNames2.stream().collect(Collectors.toList()));
+	         
+	        
 	    }
 	    
 
